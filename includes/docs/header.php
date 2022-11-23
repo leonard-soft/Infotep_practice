22cd39fbf577c9b918463581c4d25c7b69763e8c
liux-soft <112289157+leonard-soft@users.noreply.github.com>
2022-11-23 02:52:17 +0000

    ini

--- includes/docs/header.php
+++ includes/docs/header.php
@@ -26,23 +26,21 @@
 
              <!--icono de buscador e input-->
              <div class="header-left">
-                 <div class="input-group icons">
-                   <div class="input-group-prepend">
+               <div class="input-group icons">
+                 <div class="input-group-prepend">
                     <a class="input-group-text bg-transparent border-0 pr-2 pr-sm-3 btn btn-info" id="basic-addon1">
                         <i class="fa-solid fa-search"></i>
                     </a>
-                </div>
-                
-                <div class="drop-down animated flipInX d-md-none">
+                 </div>
+              
+                 <div class="drop-down animated flipInX d-md-none">
                   <form action="#">
-                     <input type="text" class="form-control" placeholder="Search">
+                      <input type="text" class="form-control" placeholder="Search">
                   </form>
-            </div>
-        </div>
-    </div>
-    <!--icono de buscador e input--> 
-           
-
+                 </div>
+               </div>
+             </div>
+             <!--icono de buscador e input-->
           
 
             <!--info_usuario-->
