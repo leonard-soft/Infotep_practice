7aaa62a550fa5908d43fb1de4eb34569c367be37
liux-soft <112289157+leonard-soft@users.noreply.github.com>
2022-11-23 02:57:01 +0000

    Update header.php

--- includes/docs/header.php
+++ includes/docs/header.php
@@ -28,9 +28,9 @@
              <div class="header-left">
                <div class="input-group icons">
                  <div class="input-group-prepend">
-                    <a class="input-group-text bg-transparent border-0 pr-2 pr-sm-3 btn btn-info" id="basic-addon1">
+                    <button type="submit" class="input-group-text bg-transparent border-0 pr-2 pr-sm-3 btn btn-info" id="basic-addon1">
                         <i class="fa-solid fa-search"></i>
-                    </a>
+                    </button>
                  </div>
               
                  <div class="drop-down animated flipInX d-md-none">
