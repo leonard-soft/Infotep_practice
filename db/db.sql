6c38312c63240ef103fb5e0607c22a41d39480f6
Ferney Ciprian <ferneyciprian2004@gmail.com>
2022-11-23 17:10:15 -0500

    login.php added

--- db/db.sql
+++ db/db.sql
@@ -1,37 +1,40 @@
-create database if not exists db_admin;
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Versión del servidor:         8.0.17 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.0.0.6468
+-- --------------------------------------------------------
 
-use db_admin;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table if not exists usuarios(
-  
-  id_usuario int(21) auto_increment primary key,
-  usuario varchar(45),
-  contraseña varchar(45)
-  
-)ENGINE=InnoDB;
 
-create table if not exists historial(
-  
-  id_historial int(21) auto_increment primary key,
-  codigo varchar(45),
-  fecha_i date,
-  fecha_f date,
-  tipo_m varchar(45),
-  problema varchar(250),
-  causas varchar(250),
-  solucion varchar(500),
-  responsable varchar(150),
-  estado_final varchar(150),
-  constraint fk_historial_practicante foreign key(practicante_id) references practicantes(id_p)
-  
-)ENGINE=InnoDB;
+-- Volcando estructura de base de datos para db_admin
+CREATE DATABASE IF NOT EXISTS `db_admin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_admin`;
 
-create table if not exists practicantes(
+-- Volcando estructura para tabla db_admin.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int(21) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `correo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `telefono` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `direccion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `contrasena` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-  id_p int(21) auto_increment primary key,
-  cedula bigint,
-  nombre varchar(45),
-  apellido varchar(45),
-  constraint cedula_p unique(cedula)
+-- La exportación de datos fue deseleccionada.
 
-)ENGINE=InnoDB;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
