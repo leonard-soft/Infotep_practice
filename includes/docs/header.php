2fe088ab45c10f9f1bd09956d173b28e90cdb936
leonard-soft <112289157+leonard-soft@users.noreply.github.com>
2022-11-23 12:22:03 -0500

    Update header.php

--- includes/docs/header.php
+++ includes/docs/header.php
@@ -1,110 +1,95 @@
-<div id="main-wrapper">
+ <div id="main-wrapper">
 
-    <!-- Nav header start-------------->
-    <div class="nav-header">
-        <div class="brand-logo">
-            <a href="index.php">
-                <span class="brand-title" style="color:white;">
-                    INFOTEP WEB PRACTICE
-                </span>
-            </a>
-        </div>
-    </div> 
-    <!-- Fin Nav Header --------------->
-
-
-    <!--header-->
-    <div class="header">    
-        <div class="header-content clearfix">
-
-            <!--menu hamburguesa-->
-            <div class="nav-control">    
-                <div class="hamburger">
-                    <span class="toggle-icon">
-                        <i class="fa-solid fa-bars"></i>
+        <!--**********************************
+            Nav header start
+        ***********************************-->
+        <div class="nav-header">
+            <div class="brand-logo">
+                <a href="index.php">
+                    <b class="logo-abbr"><img src="images/logo.png" alt=""> </b>
+                    <span class="logo-compact">INFOTEP WEB PRACTICE</span>
+                    <span class="brand-title" style="color: white;">
+                        INFOTEP WEB PRACTICE
                     </span>
-                </div>     
+                </a>
             </div>
-            <!--fin menu hamburguesa-->
+        </div>
+        <!--**********************************
+            Nav header end
+        ***********************************-->
 
 
-            <!--icono de buscador e input-->
-            <div class="header-left">
-
-                <div class="input-group icons">
-
-                    <div class="input-group-prepend">
-                        <a class="input-group-text bg-transparent border-0 pr-2 pr-sm-3 btn btn-info" id="basic-addon1">
-                            <i class="fa-solid fa-search"></i>
-                        </a>
+        <!--**********************************
+            Header start
+        ***********************************-->
+        <div class="header">    
+            <div class="header-content clearfix">
+                
+                <!--menu hamburguesa-->
+                <div class="nav-control">
+                    <div class="hamburger">
+                        <span class="toggle-icon"><i class="fa-solid fa-list"></i></span>
                     </div>
+                </div>
+                <!--fin menu hamburguesa-->
 
-                    <div class="drop-down animated flipInX d-md-none">
-                        <form action="#">
-                            <input type="text" class="form-control" placeholder="Buscar">
-                        </form>
+                <!--formulario de busqueda-->
+                <div class="header-left">
+                    <div class="input-group icons">
+                        <div class="input-group-prepend">
+                            <a class="input-group-text bg-transparent border-0 pr-2 pr-sm-3 btn btn-success" id="basic-addon1">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </a>
+                        </div>
+                        <input type="search" class="form-control" placeholder="Search Dashboard" aria-label="Search Dashboard">
+                        <div class="drop-down animated flipInX d-md-none">
+                            <form action="#">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </form>
+                        </div>
                     </div>
+                </div>
+                <!--formulario de busqueda-->
+ 
+                <!--cierre de sesion-->
+                <div class="header-right">
+                    <ul class="clearfix">
+
+                        <li class="icons dropdown d-none d-md-flex">
+                            <a href="javascript:void(0)" class="log-user"  data-toggle="dropdown">
+                                <span>Username</span>  
+                            </a>
+                        </li>
+
+
+                        <li class="icons dropdown">
+                            <div class="user-img c-pointer position-relative"   data-toggle="dropdown">
+                                <span class="activity active"></span>
+                                <img src="../img/perfil_masculino.png" height="40" width="40" alt="">
+                            </div>
+                            <div class="drop-down dropdown-profile animated fadeIn dropdown-menu">
+                                <div class="dropdown-content-body">
+                                    <ul>
+                                        <li>
+                                            <a href="app-profile.html"><i class="icon-user"></i> <span>Perfil</span></a>
+                                        </li>
+                                        
+                                        <hr class="my-2">
+
+                                       
+                                        <li>
+                                            <a href="page-login.html"><i class="icon-key"></i> <span>Logout</span></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
 
                 </div>
 
             </div>
-            <!--icono de buscador e input-->
-
-
-            <!--info_usuario-->
-            <div class="header-right">
-                <ul class="clearfix">
-
-
-                    <!--nombre_usuario-->
-                    <li class="icons dropdown d-none d-md-flex">
-                        <span>Nombre_Usuario</span>  
-                    </li>
-                    <!--fin_nombre_usuario-->
-
-
-                    <!--imagen y boton de perfil-->
-                    <li class="icons dropdown">
-                        <div class="user-img c-pointer position-relative"   data-toggle="dropdown">
-                            <span class="activity active"></span>
-
-                            <!--imagen-->
-                            <img src="../img/perfil_masculino.png" height="40" width="40" alt="">
-                            <!--imagen-->
-                        </div>
-
-                        <div class="drop-down dropdown-profile animated fadeIn dropdown-menu">
-                            div class="dropdown-content-body">
-                            <ul>
-
-                                <li>
-                                    <a href="app-profile.html"><i class="icon-user"></i> <span>Perfil</span></a>
-                                </li>
-
-
-                                <hr class="my-2">
-
-
-                                <li>
-                                    <a href="page-login.html">
-                                        <i class="icon-key"></i> 
-                                        <span>Salir</span>
-                                    </a>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <!--fin de imagen y menu del perfil-->
-
-            
-            <!--fin_info_usuario-->
-
         </div>
-
-    </div>
-
-</div>
- <!--finHeader-->
+        <!--**********************************
+            Header end ti-comment-alt
+        ***********************************-->
