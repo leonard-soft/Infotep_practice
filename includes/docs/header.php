4c3cb8909dbaba9d10aa821ec7e3421c1c410ddc
liux-soft <112289157+leonard-soft@users.noreply.github.com>
2022-11-23 02:11:02 +0000

    fine

--- includes/docs/header.php
+++ includes/docs/header.php
@@ -4,7 +4,6 @@
     <div class="nav-header">
         <div class="brand-logo">
             <a href="index.php">
-                <b class="logo-abbr"><img src="images/logo.png" alt=""></b>
                 <span class="brand-title" style="color:white;">
                     INFOTEP WEB PRACTICE
                 </span>
@@ -53,7 +52,6 @@
                 <li class="icons dropdown d-none d-md-flex">
                     <span>Nombre_Usuario</span>  
                 </li>
-<<<<<<< HEAD
                 <!--fin_nombre_usuario-->
 
 
@@ -76,91 +74,6 @@
                           <a href="app-profile.html"><i class="icon-user"></i> <span>Perfil</span></a>
                         </li>
                            
-=======
-                <li class="icons dropdown"><a href="javascript:void(0)" data-toggle="dropdown">
-                    <i class="fa-solid fa-bell"></i>
-                    <span class="badge badge-pill gradient-2">3</span>
-                </a>
-                <div class="drop-down animated fadeIn dropdown-menu dropdown-notfication">
-                    <div class="dropdown-content-heading d-flex justify-content-between">
-                        <span class="">2 New Notifications</span>  
-                        <a href="javascript:void()" class="d-inline-block">
-                            <span class="badge badge-pill gradient-2">5</span>
-                        </a>
-                    </div>
-                    <div class="dropdown-content-body">
-                        <ul>
-                            <li>
-                                <a href="javascript:void()">
-                                    <span class="mr-3 avatar-icon bg-success-lighten-2"><i class="icon-present"></i></span>
-                                    <div class="notification-content">
-                                        <h6 class="notification-heading">Events near you</h6>
-                                        <span class="notification-text">Within next 5 days</span> 
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void()">
-                                    <span class="mr-3 avatar-icon bg-danger-lighten-2"><i class="icon-present"></i></span>
-                                    <div class="notification-content">
-                                        <h6 class="notification-heading">Event Started</h6>
-                                        <span class="notification-text">One hour ago</span> 
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void()">
-                                    <span class="mr-3 avatar-icon bg-success-lighten-2"><i class="icon-present"></i></span>
-                                    <div class="notification-content">
-                                        <h6 class="notification-heading">Event Ended Successfully</h6>
-                                        <span class="notification-text">One hour ago</span>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void()">
-                                    <span class="mr-3 avatar-icon bg-danger-lighten-2"><i class="icon-present"></i></span>
-                                    <div class="notification-content">
-                                        <h6 class="notification-heading">Events to Join</h6>
-                                        <span class="notification-text">After two days</span> 
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                        
-                    </div>
-                </div>
-            </li>
-            <li class="icons dropdown d-none d-md-flex">
-                <a href="javascript:void(0)" class="log-user"  data-toggle="dropdown">
-                    <span>English</span>  <i class="fa fa-angle-down f-s-14" aria-hidden="true"></i>
-                </a>
-                <div class="drop-down dropdown-language animated fadeIn  dropdown-menu">
-                    <div class="dropdown-content-body">
-                        <ul>
-                            <li><a href="javascript:void()">English</a></li>
-                            <li><a href="javascript:void()">Dutch</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </li>
-            <li class="icons dropdown">
-                <div class="user-img c-pointer position-relative"   data-toggle="dropdown">
-                    <span class="activity active"></span>
-                    <img src="../img/user.png" height="40" width="40" alt="">
-                </div>
-                <div class="drop-down dropdown-profile animated fadeIn dropdown-menu">
-                    <div class="dropdown-content-body">
-                        <ul>
-                            <li>
-                                <a href="app-profile.html"><i class="icon-user"></i> <span>Profile</span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void()">
-                                    <i class="icon-envelope-open"></i> <span>Inbox</span> <div class="badge gradient-3 badge-pill gradient-1">3</div>
-                                </a>
-                            </li>
->>>>>>> af34edb273de6eecd4791ae44545043a77c14605
                             
                         <hr class="my-2">
                         
