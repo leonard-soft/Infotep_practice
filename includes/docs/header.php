c2e9618b84cdc528cc45008d7b2db94f6daeefe5
liux-soft <112289157+leonard-soft@users.noreply.github.com>
2022-11-23 03:10:42 +0000

    Update header.php

--- includes/docs/header.php
+++ includes/docs/header.php
@@ -21,81 +21,81 @@
                     <span class="toggle-icon"><i class="fa-solid fa-bars"></i></span>
                 </div>     
             </div>
-             <!--fin menu hamburguesa-->
+            <!--fin menu hamburguesa-->
 
 
-             <!--icono de buscador e input-->
-             <div class="header-left">
+            <!--icono de buscador e input-->
+            <div class="header-left">
 
-               <div class="input-group icons">
+             <div class="input-group icons">
 
-                 <div class="input-group-prepend">
-                    <a class="input-group-text bg-transparent border-0 pr-2 pr-sm-3 btn btn-info" id="basic-addon1">
-                        <i class="fa-solid fa-search"></i>
-                    </a>
-                 </div>
-              
-                 <div class="drop-down animated flipInX d-md-none">
-                   <form action="#">
-                      <input type="text" class="form-control" placeholder="Buscar">
-                   </form>
-                 </div>
+               <div class="input-group-prepend">
+                <a class="input-group-text bg-transparent border-0 pr-2 pr-sm-3 btn btn-info" id="basic-addon1">
+                    <i class="fa-solid fa-search"></i>
+                </a>
+            </div>
 
-              </div>
+            <div class="drop-down animated flipInX d-md-none">
+             <form action="#">
+              <input type="text" class="form-control" placeholder="Buscar">
+          </form>
+      </div>
 
-             </div>
-             <!--icono de buscador e input-->
-          
-
-             <!--info_usuario-->
-             <div class="header-right">
-              <ul class="clearfix">
-
-
-                <!--nombre_usuario-->
-                <li class="icons dropdown d-none d-md-flex">
-                    <span>Nombre_Usuario</span>  
-                </li>
-                <!--fin_nombre_usuario-->
-
-
-                <!--imagen y boton de perfil-->
-                <li class="icons dropdown">
-                 <div class="user-img c-pointer position-relative"   data-toggle="dropdown">
-                   <span class="activity active"></span>
-
-                    <!--imagen-->
-                    <img src="../img/perfil_masculino.png" height="40" width="40" alt="">
-                    <!--imagen-->
-
-                 </div>
-
-                 <div class="drop-down dropdown-profile animated fadeIn dropdown-menu">
-                   <div class="dropdown-content-body">
-                     <ul>
-
-                       <li>
-                          <a href="app-profile.html"><i class="icon-user"></i> <span>Perfil</span></a>
-                        </li>
-                           
-                            
-                        <hr class="my-2">
-                        
-
-                        <li>
-                            <a href="page-login.html"><i class="icon-key"></i> <span>Salir</span></a>
-                        </li>
-
-                      </ul>
-                    </div>
-                  </div>
-                </li>
-                <!--fin de imagen y menu del perfil-->
-
-             </ul>
-           </div>
-   <!--fin_info_usuario-->
- 
   </div>
+
+</div>
+<!--icono de buscador e input-->
+
+
+<!--info_usuario-->
+<div class="header-right">
+  <ul class="clearfix">
+
+
+    <!--nombre_usuario-->
+    <li class="icons dropdown d-none d-md-flex">
+        <span>Nombre_Usuario</span>  
+    </li>
+    <!--fin_nombre_usuario-->
+
+
+    <!--imagen y boton de perfil-->
+    <li class="icons dropdown">
+       <div class="user-img c-pointer position-relative"   data-toggle="dropdown">
+         <span class="activity active"></span>
+
+         <!--imagen-->
+         <img src="../img/perfil_masculino.png" height="40" width="40" alt="">
+         <!--imagen-->
+
+     </div>
+
+     <div class="drop-down dropdown-profile animated fadeIn dropdown-menu">
+div class="dropdown-content-body">
+<ul>
+
+<li>
+<a href="app-profile.html"><i class="icon-user"></i> <span>Perfil</span></a>
+</li>
+
+
+<hr class="my-2">
+
+
+<li>
+<a href="page-login.html"><i class="icon-key"></i> <span>Salir</span></a>
+</li>
+
+</ul>
+</div>
+</div>
+</li>
+<!--fin de imagen y menu del perfil-->
+
+
+</div>
+<!--fin_info_usuario-->
+
+</div>
 </div>
 <!--finHeader-->
